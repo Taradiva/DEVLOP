73c4aba962482ec3ea3e992a86ec7e550c1416e2
Taradiva <taradiva@students.amikom.ac.id>
2025-05-23 12:09:26 +0700

    Update tests.py

---
+++ tests.py
@@ -0,0 +1,7 @@
+from app.main import app
+
+def test_home():
+    tester = app.test_client()
+    response = tester.get("/")
+    assert response.status_code == 200
+    assert b"Halo dari Flask!" in response.data
